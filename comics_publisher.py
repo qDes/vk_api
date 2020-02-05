04f1f1b11001a123c33eeb85482fcc8e8248fd48
Andrei K <1____1@bk.ru>
2020-02-05 15:08:26 +0300

    refactoring

---
+++ comics_publisher.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import logging
+
+from dotenv import load_dotenv
+from random import randint
+from tools import download_image, fetch_xkcd_image_url
+from vk import get_wall_upload_server, save_wall_photo
+from vk import upload_photo_wall, VkError
+
+
+def post_random_xkcd_to_vk(access_token, group_id):
+    xkcd_number = randint(1, 2263)
+    image_url = fetch_xkcd_image_url(xkcd_number)
+    download_image(image_url)
+    upload_server = get_wall_upload_server(access_token, group_id)
+    save_wall = save_wall_photo(access_token, group_id, upload_server)
+    response = upload_photo_wall(access_token, group_id, save_wall)
+    post_id = response.get('response').get('post_id')
+    logging.info(f"Posted {post_id}")
+    os.remove("xkcd.jpg")
+
+
+if __name__ == "__main__":
+    logging.disable(sys.maxsize)
+    load_dotenv()
+    access_token = os.environ["ACCESS_TOKEN"]
+    group_id = os.environ["GROUP_ID"]
+    try:
+        post_random_xkcd_to_vk(access_token, group_id)
+    except VkError as e:
+        print("Ошибка:", e)
